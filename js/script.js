202f95f4e2d8689e1397cbd4909a9a07b5aa096b
jansan <jansan-26@hotmail.com>
2019-10-02 15:44:32 +0200

    Display products

--- js/script.js
+++ js/script.js
@@ -15,14 +15,50 @@ let cart = [];
 
 // getting the products
 class Products {
+    async getProducts() {
+        try {
+            let result = await fetch('products.json')
+            let data = await result.json();
 
+            let products = data.items;
+            products = products.map(item => {
+                const { title, price } = item.fields;
+                const { id } = item.sys;
+                const image = item.fields.image.fields.file.url;
+                return { title, price, id, image }
+            });
+            return products
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
 }
 
 // display products
 class UI {
-
-
+    displayProducts(products) {
+        let result = '';
+        products.forEach(product => {
+            result += `
+            <!-- single product -->
+      <article class="product">
+        <div class="img-container">
+          <img src=${product.image}>
+          <button class="bag-btn" data-id=${product.id}>
+            <i class="fas fa-shopping-cart">
+              add to bag
+            </i>
+          </button>
+        </div>
+        <h3>${product.title}</h3>
+        <h4>$${product.price}</h4>
+      </article>
+      <!-- end single product -->
+            `;
+        });
+        productsDOM.innerHTML = result;
+    }
 }
 
 //local storage
@@ -34,4 +70,8 @@ class Storage {
 document.addEventListener("DOMContentLoaded", () => {
     const ui = new UI();
     const products = new Products();
-})
+
+    // get all products
+    products.getProducts().then(products =>
+        ui.displayProducts(products))
+});
